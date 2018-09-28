a474d47b6d204a15cd0c06dded22bfb1de8071aa
trimurthypanchal <43563443+trimurthypanchal@users.noreply.github.com>
2018-09-29 00:21:26 +0530

    topdowncontroller

--- 2DController/Assets/Scripts/PlayerController.cs
+++ 2DController/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+    public float speed;                                   //top down contrroller
+    public Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+	
 	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
 		
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
+
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));        //smooth movement
+
+        //Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));   //for snappy movement
+
+        moveVelocity = moveInput.normalized * speed;
 		
 	}
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
 }
